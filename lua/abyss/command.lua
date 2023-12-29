e154c4c0bfb066e02793788e39f8c9e22b035fb5
MohammadAlimul <mohammad4limul@gmail.com>
2023-12-29 16:22:16 +0700

    rsync

--- lua/abyss/command.lua
+++ lua/abyss/command.lua
@@ -11,9 +11,26 @@ function ChangeDirectory(dir)
     vim.cmd('e '..dir)
 end
 
+function RsyncCWD()
+    vim.cmd('w')
+    local cwd = vim.fn.getcwd()
+    local directory_name = vim.fn.fnamemodify(cwd, ":p:h:t")
+    local remote_username = os.getenv("REMOTE_USERNAME")
+    local remote_ip_address = os.getenv("REMOTE_IP_ADDRESS")
+    local remote_ssh_key_path = os.getenv("REMOTE_SSH_KEY_PATH")
+    local cmd = 'rsync -e "ssh -i \''..remote_ssh_key_path..'\'" --delete-after --exclude /dist --filter="P /target" --filter=":e- .gitignore" --filter "- .git/" -v -a ../'..directory_name..'/ '..remote_username..'@'..remote_ip_address..'~/abyss/sync/'..directory_name
+    local output = vim.fn.systemlist(cmd)
+    for _, line in ipairs(output) do
+        print(line)
+    end
+end
+
 vim.keymap.set("n", "<leader>db", ":lua DeleteBuffer()<cr>")
 
 vim.cmd('command! W w')
+
+vim.cmd('command! Wr lua RsyncCWD()')
+vim.cmd('command! WR lua RsyncCWD()')
 
 vim.cmd('command! Wt vs | wincmd l | edit C:/Users/user/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json')
 
